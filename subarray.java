604555c87cd66d8219ddb9ebed1400d860cd49d4
Varchala Ganugapati <gvarchala@gmail.com>
2017-09-23 20:32:34 +0530

    Create subarray.java

---
+++ subarray.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        int c=0,sum=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+          sum=0;
+            for(int j=i;j<n;j++) //6 7 -8 -9 7
+            {
+                sum=sum+a[j];
+  
+            if(sum<0)
+            {
+                c++;
+            }
+            }
+        }
+        System.out.println(c);
+    }
+}
